5280894ec82593234e8ee6bf903b5852ee711842
Hiram <hculebro@neodata.com.mx>
2023-04-06 00:44:09 -0600

    por pais busqueda added

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,5 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './por-pais.component.html'
 })
 export class PorPaisComponent {
+  termino: string = '';
 
+  buscar() {
+    console.log(this.termino)
+  }
 }
