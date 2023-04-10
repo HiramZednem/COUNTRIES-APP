c491fb9a8ff91bb4cc3de77f5c67ffbb7b37f283
Hiram <hculebro@neodata.com.mx>
2023-04-10 10:06:49 -0600

    se optimizaron las petciones http

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable} from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
 
@@ -9,21 +9,24 @@ import { Country } from '../interfaces/pais.interface';
 export class PaisService {
   private api: string = 'https://restcountries.com/v3.1/';
 
+  get httpParams () {
+    return new HttpParams().set('fields', 'name,flags,capital,population,cca2')
+  }
   constructor( private httpClient: HttpClient ) { }
 
   buscarPais( termino: string ): Observable<Country[]> {
     const url = `${ this.api }/name/${ termino }`;
-    return this.httpClient.get<Country[]>( url );
+    return this.httpClient.get<Country[]>( url, {params: this.httpParams});
   }
 
   buscarCapital( termino: string ): Observable<Country[]> {
     const url = `${ this.api }/capital/${ termino }`;
-    return this.httpClient.get<Country[]>( url );
+    return this.httpClient.get<Country[]>( url, {params: this.httpParams} );
   }
 
   buscarRegion( region: string ): Observable<Country[]> {
     const url = `${ this.api }/region/${ region }`;
-    return this.httpClient.get<Country[]>( url );
+    return this.httpClient.get<Country[]>( url, {params: this.httpParams} );
   }
 
   buscarPaisPorCodigo( id: string ) {
